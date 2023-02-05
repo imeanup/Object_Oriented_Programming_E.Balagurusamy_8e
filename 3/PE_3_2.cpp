3e51d4b91fe8ebe56f191b25e63cf71e93ae1c48
anupam <tah.anupam@gmail.com>
2023-02-05 22:13:33 +0530

    PE_3_2

--- 3/PE_3_2.cpp
+++ 3/PE_3_2.cpp
@@ -1,1 +1,17 @@
 // write a function that creates a vector of user-given size M using new operator.
+#include <iostream>
+#include <vector>
+
+std::vector<int>* create_vector(int M) {
+    std::vector<int>* v = new std::vector<int>(M);
+    return v;
+}
+
+int main() {
+    int M;
+    std::cout << "Enter the size of the vector: ";
+    std::cin >> M;
+    std::vector<int>* v = create_vector(M);
+    delete v;
+    return 0;
+}
