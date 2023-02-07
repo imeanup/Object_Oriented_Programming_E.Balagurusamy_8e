37f068cd6264c44a38364b5885f627e19fc41170
anupam <tah.anupam@gmail.com>
2023-02-07 15:13:01 +0000

    7 Feb 2023, 20:43

---
+++ Debugging Exercises/5/5_2.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+class stm{
+    static int a;
+    int number;
+public:
+    void increment(int b){
+        number = b;
+        a++;
+    }
+    void show(void){
+        cout << "a: " << a << endl;
+    }
+};
+int stm :: a;
+
+int main(){
+    stm st1,st2,st3;
+    st1.increment(100); // No value passed in the function
+    st1.show();
+    st2.show();
+    st3.show();
+    return 0;
+}
