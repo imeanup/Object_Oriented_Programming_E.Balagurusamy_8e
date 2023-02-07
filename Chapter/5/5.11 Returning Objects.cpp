1e3e4002ca5328962e0aa6ea0d546d0a0d3cc9f0
anupam <tah.anupam@gmail.com>
2023-02-07 19:19:42 +0530

    5.11 Returning Objects

--- Chapter/5/5.11 Returning Objects.cpp
+++ Chapter/5/5.11 Returning Objects.cpp
@@ -32,7 +32,7 @@ matrix trans(matrix m1){
     int i, j;
     for(i = 0; i < 3; i++){
         for(j = 0; j < 3; j++){
-            m2.m[i][j] = m1.m[i][j];
+            m2.m[i][j] = m1.m[j][i];
         }
     }
     return(m2);
@@ -74,8 +74,8 @@ You entered the following matrix:
 7       8       9
 
 Transposed matrix: 
-1       2       3
-4       5       6
-7       8       9  
+1       4       7
+2       5       8
+3       6       9 
 
 */
