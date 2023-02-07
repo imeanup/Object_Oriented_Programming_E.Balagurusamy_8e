93d2d9da68687b71ff44c09f01d066130c18b9ba
anupam <tah.anupam@gmail.com>
2023-02-07 06:34:38 +0000

    7 Feb 2023, 12:04

--- Chapter/5/5.3 Processing Shopping List.cpp
+++ Chapter/5/5.3 Processing Shopping List.cpp
@@ -29,5 +29,56 @@ void ITEMS :: getitem(void)
 
 void ITEMS :: displaySum()
 {
-    
+    float sum = 0;
+    for (int i = 0; i < count; i++){
+        sum += itemPrice[i];
+        cout << "\nTotal Value: " << sum << "\n";
+    }
+}
+
+void ITEMS :: remove(){
+    int a;
+    cout << "Enter item code: ";
+    cin >> a;
+    for (int i = 0; i < count; i++){
+        if (itemCode[i] == a){
+            itemPrice[i] = 0;
+        }
+    }
+}
+
+void ITEMS :: displayItems(void){
+    cout << "\nCode Price\n";
+    for (int i = 0; i < count; i++){
+        cout << "\n" << itemCode[i];
+        cout << " " << itemPrice[i];
+    }
+    cout << "\n";
+}
+
+int main(){
+    ITEMS order;
+    order.CNT();
+    int x;
+    do {
+        cout << "\nYou can do the following;"
+        <<"Enter appropriate number\n";
+        cout << "\n1: Add an item";
+        cout << "\n2: Display total value";
+        cout << "\n3: Delete an item";
+        cout << "\n4: Display all items";
+        cout << "\n5: Quit";
+        cout << "\n\nWhat is your option? ";
+
+        cin >> x;
+        switch(x){
+            case 1: order.getitem(); break;
+            case 2: order.displaySum(); break;
+            case 3: order.remove(); break;
+            case 4: order.displayItems(); break;
+            case 5: break;
+            default : cout << "Error in input; try again\n";
+        }
+    } while(x != 5);
+    return 0;
 }
