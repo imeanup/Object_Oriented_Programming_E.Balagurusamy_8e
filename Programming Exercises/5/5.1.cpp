57bbf6018aa336780e0c68212942052c18517df9
anupam <tah.anupam@gmail.com>
2023-02-08 09:12:26 +0000

    8 Feb 2023, 14:42

---
+++ Programming Exercises/5/5.1.cpp
@@ -0,0 +1,16 @@
+/*
+Define a lass to represent a bank account. Inlcude the following members:
+Data members
+a) Name of the depositor
+b) Account number
+c) Type of account
+d) Balance amount in the account
+
+Member functions:
+a) To assign initial values
+b) To deposit an amount
+c) To withdraw an amount after checking the balance
+d) To display name and balance
+
+Write a main program to test the program.
+*/
