82b85f5b730f0362156cd3dc1ac410c2d217a254
anupam <tah.anupam@gmail.com>
2023-02-08 15:59:46 +0000

    8 Feb 2023, 21:29

--- Programming Exercises/4/4.5.cpp
+++ Programming Exercises/4/4.5.cpp
@@ -4,5 +4,9 @@ using namespace std;
 #define MAX(a, b, c) ( (a) > (b) ? ((a) > (c) ? (a) : (c)) : ((b) > (c) ? (b) : (c)) )
 
 int main(){
-    
+    int a, b, c;
+    cout << "Enter three numbers" << endl;
+    cin >> a >> b >> c;
+    cout << "The largest of three number is " << MAX(a, b, c) << endl;
+    return 0;
 }
