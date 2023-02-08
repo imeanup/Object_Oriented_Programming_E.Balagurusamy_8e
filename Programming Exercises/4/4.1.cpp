c12656b77abdd120635e84b210cfe5fb8e2249f8
anupam <tah.anupam@gmail.com>
2023-02-08 14:51:04 +0000

    8 Feb 2023, 20:21

--- Programming Exercises/4/4.1.cpp
+++ Programming Exercises/4/4.1.cpp
@@ -1,3 +1,9 @@
 #include<iostream>
 using namespace std;
 
+int main(){
+    int row, col;
+    cout << "Enter matrix size: row column ";
+    cin >> row >> col;
+    cout << row << " x " << col <<  endl;
+}
