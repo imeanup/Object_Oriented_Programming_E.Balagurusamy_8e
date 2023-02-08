73cbf25ccc27b48b9beef4dac7e9d7f1377b85fa
anupam <tah.anupam@gmail.com>
2023-02-08 15:33:22 +0000

    8 Feb 2023, 21:03

---
+++ Programming Exercises/4/4.4.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+void printArea(int x, int y){
+    cout << "The area of a rectangle is "<< x*y << endl;
+}
+
+void printArea(int a){
+    cout << "The area of a square is " << a*a << endl;
+}
+
+void printArea(int r){
+    cout << "The area of a circle is " << 3.14*r*r << endl;
+}
+
+int main(){
+    printArea(10, 20);
+    printArea(4);
+    printArea(3);
+    return 0;
+}
