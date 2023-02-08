79f0a0fb4cf6ae4307c379bdbcc4773948a8fa0d
anupam <tah.anupam@gmail.com>
2023-02-08 14:44:19 +0530

    PE 5.1.cpp

--- Programming Exercises/5/5.1.cpp
+++ Programming Exercises/5/5.1.cpp
@@ -14,3 +14,57 @@ d) To display name and balance
 
 Write a main program to test the program.
 */
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+class BankAccount {
+    string name;
+    int accountNumber;
+    string type;
+    float balance;
+
+public:
+    BankAccount(string name, int accountNumber, string type, float balance) {
+        this->name = name;
+        this->accountNumber = accountNumber;
+        this->type = type;
+        this->balance = balance;
+    }
+
+    void deposit(float amount) {
+        balance += amount;
+    }
+
+    void withdraw(float amount) {
+        if (balance >= amount) {
+            balance -= amount;
+            cout << "Withdrawal successful." << endl;
+        } else {
+            cout << "Insufficient balance." << endl;
+        }
+    }
+
+    void display() {
+        cout << "Name: " << name << endl;
+        cout << "Account Number: " << accountNumber << endl;
+        cout << "Type: " << type << endl;
+        cout << "Balance: " << balance << endl;
+    }
+};
+
+int main() {
+    BankAccount account("John Doe", 12345, "Savings", 5000);
+    cout << "Initial details: " << endl;
+    account.display();
+    account.deposit(2000);
+    cout << "\nAfter deposit: " << endl;
+    account.display();
+    account.withdraw(3000);
+    cout << "\nAfter withdrawal: " << endl;
+    account.display();
+
+    return 0;
+}
