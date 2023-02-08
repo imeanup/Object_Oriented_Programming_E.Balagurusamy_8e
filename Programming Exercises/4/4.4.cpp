eaae220f90a54d550f8197b2b8f3ce5fb9d2f159
anupam <tah.anupam@gmail.com>
2023-02-08 15:54:18 +0000

    8 Feb 2023, 21:24

--- Programming Exercises/4/4.4.cpp
+++ Programming Exercises/4/4.4.cpp
@@ -1,22 +1,16 @@
 #include<iostream>
 using namespace std;
 
-void printArea(int x, int y){
-    cout << "The area of a square is " << x*y << endl;
+void printArea(double r){
+    cout << "The area of a circle is " << 3.14*r*r << endl;
 }
-
-// void printArea(double r){
-//     cout << "The area of a circle is " << 3.14*r*r << endl;
-// }
 
 void printArea(int x, int y = 20){
     cout << "The area of a rectangle is "<< x*y << endl;
 }
 
 int main(){
-    
-    // printArea(4);
     printArea(3.0);
-    // printArea(10);
+    printArea(10);
     return 0;
 }
