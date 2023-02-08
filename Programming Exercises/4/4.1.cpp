9f916ca7b4bc0d262fa989042a4005de1453670b
anupam <tah.anupam@gmail.com>
2023-02-08 15:10:09 +0000

    8 Feb 2023, 20:40

--- Programming Exercises/4/4.1.cpp
+++ Programming Exercises/4/4.1.cpp
@@ -1,36 +1,44 @@
 #include <iostream>
+#include <iomanip>
+#include<vector>
 using namespace std;
 
-void readMatrix(int m, int n, int** matrix) {
-  for (int i = 0; i < m; i++) {
-    for (int j = 0; j < n; j++) {
-      cout << "Enter the element at row " << i + 1 << " and column " << j + 1 << ": ";
-      cin >> matrix[i][j];
+void matrix(int m, int n)
+{
+    /*
+    float **p;
+    p = new float*[m];
+    for (int i = 0; i < m; i++)
+    {
+        p[i] = new float[n];
     }
-  }
+    */
+    vector<vector<int> > p(m, vector<int>(n));
+
+    cout << "Enter " << m << " by " << n << " matrix elements one by one" << endl;
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> p[i][j];
+        }
+    }
+    cout << "The given matrix is:" << endl;
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << p[i][j] << " ";
+        }
+        cout << endl;
+    }
 }
 
-int main() {
-  int m, n;
-  cout << "Enter the number of rows in the matrix: ";
-  cin >> m;
-  cout << "Enter the number of columns in the matrix: ";
-  cin >> n;
-
-  // Allocating memory for the matrix
-  int** matrix = new int*[m];
-  for (int i = 0; i < m; i++) {
-    matrix[i] = new int[n];
-  }
-
-  // Reading the matrix from the keyboard
-  readMatrix(m, n, matrix);
-
-  // Deallocating memory for the matrix
-  for (int i = 0; i < m; i++) {
-    delete[] matrix[i];
-  }
-  delete[] matrix;
-
-  return 0;
+int main()
+{
+    int r, c;
+    cout << "Enter size of matrix: ";
+    cin >> r >> c;
+    matrix(r, c);
+    return 0;
 }
