f1a2bf29d2fb6bc73378c33efb68bed927a33292
anupam <tah.anupam@gmail.com>
2023-02-08 14:55:06 +0000

    8 Feb 2023, 20:25

--- Programming Exercises/4/4.1.cpp
+++ Programming Exercises/4/4.1.cpp
@@ -1,11 +1,40 @@
-#include<iostream>
+#include <iostream>
+#include <iomanip>
+
 using namespace std;
-void matrix(int m, int n){
-    
+
+void matrix(int m, int n)
+{
+    float **p;
+    p = new float*[m];
+    for (int i = 0; i < m; i++)
+    {
+        p[i] = new float[n];
+    }
+    cout << "Enter " << m << " by " << n << " matrix elements one by one" << endl;
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> p[i][j];
+        }
+    }
+    cout << "The given matrix is:" << endl;
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << p[i][j] << " ";
+        }
+        cout << endl;
+    }
 }
-int main(){
-    int row, col;
-    cout << "Enter matrix size: row column ";
-    cin >> row >> col;
-    cout << row << " x " << col <<  endl;
+
+int main()
+{
+    int r, c;
+    cout << "Enter size of matrix: ";
+    cin >> r >> c;
+    matrix(r, c);
+    return 0;
 }
