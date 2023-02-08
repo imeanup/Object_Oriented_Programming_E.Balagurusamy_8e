a415c03fd0c60d83d8f9a77c6e580281283c0a56
anupam <tah.anupam@gmail.com>
2023-02-08 16:05:08 +0000

    8 Feb 2023, 21:35

--- Programming Exercises/4/4.5.cpp
+++ Programming Exercises/4/4.5.cpp
@@ -2,6 +2,10 @@
 using namespace std;
 
 /*
+To understand ternary operator, use two elements first:
+#define MAX(a, b) ( (a) > (b) ? (a) : (b) )
+Now, use the below.
+
 Using ternary operator to check the value of (a) against (b) and (c)
 If (a) > (b), then it checks (a) against (c) and returns (a) if it is greater. 
 If (b) > (a), then it checks (b) against (c) and returns (b) if it is greater.
