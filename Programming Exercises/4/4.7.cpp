2a63b4e68c7546d5b49ea488f66e447b4fea9af5
anupam <tah.anupam@gmail.com>
2023-02-08 16:15:56 +0000

    8 Feb 2023, 21:45

--- Programming Exercises/4/4.7.cpp
+++ Programming Exercises/4/4.7.cpp
@@ -1,11 +1,21 @@
 #include<iostream>
 using namespace std;
 
-long double power(double m, int n = 2){
-
+double power(double m, int n = 2){
+    double result = 1;
+    for (int i = 0; i < n; i++){
+        result *= m;
+    }
+    return result;
 }
 
 int main(){
-    int x, y;
-
+    double x;
+    int y;
+    cout << "Enter a number: " << endl;
+    cin >> x;
+    cout << "Enter the power (default = 2): ";
+    cin >> y;
+    cout << "Result: " << power(x, y) << endl;
+    return 0;
 }
