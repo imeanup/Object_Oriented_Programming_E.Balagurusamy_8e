9b6cc23d529a319f13a84f3062c510d01b6a9a48
anupam <tah.anupam@gmail.com>
2023-02-09 13:44:50 +0530

    3.1

--- Debugging Exercises/3/3.1.cpp
+++ Debugging Exercises/3/3.1.cpp
@@ -3,6 +3,6 @@ using namespace std;
 
 int main(){
     int num[] = {1, 2, 3, 4, 5, 6};
-    num[1] = num[1] ? cout << "Success" : cout << "Error";
+    num[1] == num[1] ? cout << "Success" << endl : cout << "Error" << endl;
     return 0;
 }
