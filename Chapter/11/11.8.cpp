a06bd53248b3305288395422ed410d6424dd6f4d
anupam <tah.anupam@gmail.com>
2023-02-10 10:49:08 +0000

    10 Feb 2023, 16:19

--- Chapter/11/11.8.cpp
+++ Chapter/11/11.8.cpp
@@ -34,7 +34,47 @@ int main(){
     item.getdata();
     char ch;
     cin.get(ch);
-    while(inoutfile.write((char*) & item, sizeof item)){
-        
+    inoutfile.write((char*) & item, sizeof item);
+
+    inoutfile.seekg(0);
+    cout << "CONTENTS of APPENDED FILE\n";
+
+    while(inoutfile.read((char*) & item, sizeof item)){
+        item.putdata();
     }
+
+    int last = inoutfile.tellg();
+    int n = last/sizeof(item);
+
+    cout << "Number of objects = " << n << "\n";
+    cout << "Total bytes in the file = " << last << "\n";
+
+    cout << "Enter object number to be updated \n";
+    int object;
+    cin >> object;
+    cin.get(ch);
+
+    int location = (object - 1) * sizeof(item);
+    if (inoutfile.eof())
+    inoutfile.clear();
+
+    inoutfile.seekp(location);
+
+    cout << " Enter new values of the Object\n";
+    item.getdata();
+    cin.get(ch);
+
+    inoutfile.write((char*) & item, sizeof item) << flush;
+
+    inoutfile.seekg(0);
+
+    cout << "CONTENTS of UPDATED FILE\n";
+
+    while (inoutfile.read((char*)& item, sizeof item)){
+        item.putdata();
+    }
+
+    inoutfile.close();
+
+    return 0;
 }
