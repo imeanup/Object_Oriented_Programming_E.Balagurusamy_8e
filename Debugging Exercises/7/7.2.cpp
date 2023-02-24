d5633626d5f38d3aff639141dc8da3f43f68741d
anupam <tah.anupam@gmail.com>
2023-02-24 18:20:59 +0000

    24 Feb 2023, 23:50

--- Debugging Exercises/7/7.2.cpp
+++ Debugging Exercises/7/7.2.cpp
@@ -17,17 +17,18 @@ enum WeekDays{
 };
 
 bool operator==(WeekDays w1, WeekDays w2){
+    // return w1 == w2;
     if (w1== mSunday && w2 == mSunday)
         return 1;
-    else if (w1== mSunday && w2 == mSunday)
+    else if (w1 == mSunday && w2 == mSunday)
         return 1;
-    else if (w1== mSunday && w2 == mSunday)
+    else if (w1 == mSunday && w2 == mSunday)
         return 1;
-    else if (w1== mSunday && w2 == mSunday)
+    else if (w1 == mSunday && w2 == mSunday)
         return 1;
-    else if (w1== mSunday && w2 == mSunday)
+    else if (w1 == mSunday && w2 == mSunday)
         return 1;
-    else if (w1== mSunday && w2 == mSunday)
+    else if (w1 == mSunday && w2 == mSunday)
         return 1;
     else
         return 0;
