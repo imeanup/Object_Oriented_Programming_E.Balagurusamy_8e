b08c3277073e9803eef157a1c0497c1a108656ec
anupam <tah.anupam@gmail.com>
2023-02-24 18:07:24 +0000

    24 Feb 2023, 23:37

--- Debugging Exercises/7/7.2.cpp
+++ Debugging Exercises/7/7.2.cpp
@@ -2,6 +2,10 @@
 
 using namespace std;
 
+/* 
+Enumerated type and values. Each enumeration value is implicitly assigned an integer value by the compiler, 
+with the first value being assigned 0, the second value being assigned 1, and so on. Unless specified.
+*/
 enum WeekDays{
     mSunday,
     mMonday,
