350d1f90365f90d28c181d63eabb8eb7875802d8
anupam <tah.anupam@gmail.com>
2023-02-24 18:49:32 +0000

    25 Feb 2023, 00:19

--- Programming Exercises/7/1.cpp
+++ Programming Exercises/7/1.cpp
@@ -1,3 +1,17 @@
 #include <iostream>
 using namespace std;
 
+class FLOAT{
+    float data;
+    public:
+        FLOAT();
+        FLOAT(float d = 0.0) : data(d) {}
+
+        FLOAT operator+(FLOAT other);
+        FLOAT operator-(FLOAT other);
+        FLOAT operator*(FLOAT other);
+        FLOAT operator/(FLOAT other);
+        void display(){
+            cout << data << endl;
+        }
+};
