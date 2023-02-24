e12189193cd71716bc034ab67de46e1c2c68c1c2
anupam <tah.anupam@gmail.com>
2023-02-25 02:18:41 +0530

    7.4

--- Programming Exercises/7/4.cpp
+++ Programming Exercises/7/4.cpp
@@ -1,3 +1,59 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
+class String{
+    char *p;
+
+    public:
+        String(){p = 0;}
+        String(const char *s);
+        String(const String &s);
+        ~String();
+        friend bool operator==(const String &s, const String &t);
+};
+
+String :: String(const char *s){
+    p = new char[strlen(s) + 1];
+    strcpy(p, s);
+}
+
+String :: String(const String &s){
+    p = new char[strlen(s.p) + 1];
+    strcpy(p, s.p);
+}
+
+String :: ~String(){
+    delete[] p;
+}
+
+bool operator==(const String &s, const String &t) {
+    return strcmp(s.p, t.p) == 0;
+}
+
+int main(){
+    String s1 = "New";
+    String s2 = "York";
+    String s3 = "Delhi";
+    String s4 = "New";
+
+    if (s1 == s2) {
+        cout << "s1 and s2 are equal" << endl;
+    } else {
+        cout << "s1 and s2 are not equal" << endl;
+    }
+
+    if (s1 == s3) {
+        cout << "s1 and s3 are equal" << endl;
+    } else {
+        cout << "s1 and s3 are not equal" << endl;
+    }
+    if (s1 == s4) {
+        cout << "s1 and s4 are equal" << endl;
+    }
+    else{
+        cout << "Not equal " << endl;
+    }
+
+    return 0;
+}
