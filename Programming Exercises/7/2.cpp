939a370a0f486542f7dd2db07f7346a2f8db2449
anupam <tah.anupam@gmail.com>
2023-02-24 19:36:31 +0000

    25 Feb 2023, 01:06

--- Programming Exercises/7/2.cpp
+++ Programming Exercises/7/2.cpp
@@ -18,7 +18,7 @@ public:
         double x = x1 + x2;
         double y = y1 + y2;
         double radius = sqrt(x * x + y * y);
-        double angle = atan2(y, x) * 180 / M_PI;
+        double angle = atan2(y, x) * 180 / M_PI; // https://cplusplus.com/reference/cmath/atan2/
         if (angle < 0){
             angle += 360;
         }
