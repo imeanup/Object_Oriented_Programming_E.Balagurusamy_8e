8bdc9da120d14a6a5189b822dc4b1c215a168f97
anupam <tah.anupam@gmail.com>
2023-02-24 18:13:27 +0000

    24 Feb 2023, 23:43

--- Debugging Exercises/7/7.2.cpp
+++ Debugging Exercises/7/7.2.cpp
@@ -17,12 +17,12 @@ enum WeekDays{
 };
 
 bool operator==(WeekDays w1, WeekDays w2){
-    return w1 == w2;
+    return w1==w2;
 }
 
 int main(){
     WeekDays w1 = mSunday, w2 = mSunday;
-    if (w1 == w2)
+    if (w1==w2)
         cout << "Same Day\n";
     else
         cout << "Different Day\n";
