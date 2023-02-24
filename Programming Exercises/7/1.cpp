763e7da98d8bd3bacfd9db01742def51f0ebd00f
anupam <tah.anupam@gmail.com>
2023-02-24 18:56:09 +0000

    25 Feb 2023, 00:26

--- Programming Exercises/7/1.cpp
+++ Programming Exercises/7/1.cpp
@@ -4,7 +4,6 @@ using namespace std;
 class FLOAT{
     float data;
     public:
-        FLOAT();
         FLOAT(float d = 0.0) : data(d) {}
 
         FLOAT operator+(FLOAT other);
@@ -12,6 +11,49 @@ class FLOAT{
         FLOAT operator*(FLOAT other);
         FLOAT operator/(FLOAT other);
         void display(){
-            cout << data << endl;
+            cout << "\nValue = " << data << endl;
         }
 };
+
+FLOAT FLOAT :: operator+(FLOAT other){
+    return FLOAT(data + other.data);
+}
+
+FLOAT FLOAT :: operator-(FLOAT other){
+    return FLOAT(data - other.data);
+}
+
+FLOAT FLOAT :: operator*(FLOAT other){
+    return FLOAT(data + other.data);
+}
+
+FLOAT FLOAT :: operator/(FLOAT other){
+    if (other.data == 0) {
+            cerr << "Error: Division by zero\n";
+            return FLOAT();
+        }
+    return FLOAT(data / other.data);
+}
+
+int main(){
+    FLOAT a(3.2), b(1.1), c;
+    c = a + b;
+    c.display();
+    c = a - b;
+    c.display();
+    c = a * b;
+    c.display();
+    c = a / b;
+    c.display();
+    return 0;
+}
+
+/*
+Value = 4.3
+
+Value = 2.1
+
+Value = 4.3
+
+Value = 2.90909
+*/
