7aa38324e5bf0529d0f8bbc01391ee72555cddf3
anupam <tah.anupam@gmail.com>
2023-02-25 17:30:15 +0000

    25 Feb 2023, 23:00

--- Debugging Exercises/8/8_1.cpp
+++ Debugging Exercises/8/8_1.cpp
@@ -1,44 +1,38 @@
 #include <iostream>
 using namespace std;
 
-class faculty{
-    private:
-        int fid;
-        char name[20];
-    public:
-        void getdata(){
-            cout << "Enter faculty id:- \t";
-            cin >> fid;
-            cout << "Enter Name:- \t";
-            cin >> name;
-        }
-        // int get_fid(){
-        //     return fid;
-        // }
-        // char get_name(){
-        //     cout << name << endl;
-        // }
+class faculty {
+private:
+    int fid;
+    char name[20];
+public:
+    void getdata() {
+        cout << "Enter faculty id: ";
+        cin >> fid;
+        cout << "Enter Name: ";
+        cin >> name;
+    }
 };
 
-class details : faculty{
-    public:
-        int age, exp;
-        void getdetails(){
-            getdata();
-            cout << "Enter age:- \t";
-            cin>>age;
-            cout<<"Enter years of experience:- \t";
-            cin>>exp;
-        }
-        void display(){
-            cout << "\n\n" << "faculty id\t"<<fid<<endl;
-            cout<<"Name of faculty\t"<<name<<endl;
-            cout << "Faculty age\t" << age<<endl;
-            cout << "Faculty years of experience\t"<<exp<<endl;
-        }
+class details : public faculty {
+public:
+    int age, exp;
+    void getdetails() {
+        getdata();
+        cout << "Enter age: ";
+        cin >> age;
+        cout << "Enter years of experience: ";
+        cin >> exp;
+    }
+    void display() {
+        cout << "\n\n" << "faculty id\t" << fid << endl;
+        cout << "Name of faculty\t" << name << endl;
+        cout << "Faculty age\t" << age << endl;
+        cout << "Faculty years of experience\t" << exp << endl;
+    }
 };
 
-int main(){
+int main() {
     details m;
     m.getdetails();
     m.display();
