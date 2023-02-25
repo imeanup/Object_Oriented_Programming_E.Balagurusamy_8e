0a320285debce2af3510375f4fef3dec262159b6
anupam <tah.anupam@gmail.com>
2023-02-25 18:20:24 +0000

    25 Feb 2023, 23:50

---
+++ Debugging_Dry-Run Exercises/8/8.2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class B{
+    int x;
+    public:
+        B(){cout << "1\t";}
+};
+
+class D: public B{
+    int y;
+    public:
+        D(){cout<< "2\t";}
+        D(int i){cout << "3\t";}
+};
+
+int main(){
+    B b;
+    D d1;
+    D d2(1);
+}
+
+/*
+1       1       2       1       3 
+*/
