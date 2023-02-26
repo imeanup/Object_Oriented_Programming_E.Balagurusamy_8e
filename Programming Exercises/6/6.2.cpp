38fa1a4d616d897ae8c74570c253f9d73dce7ba7
anupam <tah.anupam@gmail.com>
2023-02-26 19:54:24 +0000

    27 Feb 2023, 01:24

--- Programming Exercises/6/6.2.cpp
+++ Programming Exercises/6/6.2.cpp
@@ -1,3 +1,38 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 
+class String{
+    char* data;
+    size_t length;
+
+    public:
+        String(){
+            data = nullptr;
+            length = 0;
+        }
+        String(const char* str){
+            if (data != nullptr){
+                length = strlen(str);
+                data = new char[length + 1];
+                strcpy(data, str);
+            }
+            else{
+                data = nullptr;
+                length = 0;
+            }
+        }
+        String(const String& other) {
+            length = other.length;
+            if (other.data != nullptr) {
+                data = new char[length + 1];
+                std::strcpy(data, other.data);
+            } else {
+                data = nullptr;
+            }
+        }
+        ~String() {
+            delete[] data;
+        }
+        
+};
