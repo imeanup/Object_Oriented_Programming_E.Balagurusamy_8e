de30820c11cfedc6d34a9ab3f4f54ce2892964db
anupam <tah.anupam@gmail.com>
2023-02-26 19:21:51 +0000

    27 Feb 2023, 00:51

--- Debugging_Dry-Run Exercises/6/6_3.cpp
+++ Debugging_Dry-Run Exercises/6/6_3.cpp
@@ -5,6 +5,7 @@ class des{
         des(int x){
             cout << "start value " << x << endl;
         }
+        // destructor never takes an argument
         ~des(){
             cout << "hi" << endl;
         }
@@ -14,3 +15,7 @@ int main(){
     des d1(9);
     return 0;
 }
+/*
+start value 9
+hi
+*/
