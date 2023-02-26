7f24f78835d302f9c0768db44fc212f54cf471b1
anupam <tah.anupam@gmail.com>
2023-02-26 10:11:44 +0000

    26 Feb 2023, 15:41

--- Programming Exercises/8/5.cpp
+++ Programming Exercises/8/5.cpp
@@ -3,54 +3,62 @@
 using namespace std;
 
 class Person{
-    protected:
+    private:
         string name;
         int code;
     public:
-        void getName(string n){
-            name = n;
+        Person(string name, int code): name(name), code(code) {}
+        string getName() const{
+            return name;
         }
-        void getCode(int c){
-            code = c;
-        }
-        void display(){
-            cout << "Name: " << name << endl;
-            cout << "Code: " << code << endl;
+        int getCode() const{
+            return code;
         }
 };
 
-class Account : virtual public Person{
+class Account{
     protected:
         float pay;
     public:
-        void getPay(float p){
-            pay = p;
-        }
-        void diplayPay(){
-            cout << "Pay: " << pay << endl;
+        Account(float pay): pay(pay) {}
+        float getPay() const{
+            return pay;
         }
 };
 
 class Admin : virtual public Person{
     protected:
-        float experience;
+        int experience;
     public:
-        void getExperience(float e){
-            experience = e;
-        }
-        void displayExperience(){
-            cout << "Experience: " << experience << endl;
+        Admin(string name, int code, int experience) : Person(name, code), experience(experience){}
+        int getExperience() const{
+            return experience;
         }
 };
 
 class Master : public Account, public Admin{
+    private:
+        // No declaration
     protected:
         // No declaration
     public:
-
+        Master(string name, int code, int experience, float pay) : Person(name, code), Account(pay), Admin(name, code, experience){}
+        void display(){
+            cout << "Name: " << getName() << endl;
+            cout << "Code: " << getCode() << endl;
+            cout << "Experience:  " << getExperience() << endl;
+            cout << "Pay: " << getPay() << endl;
+        }
+        void update(int experience, float pay) {
+            Admin::experience = experience;
+            Account::pay = pay;
+    }
 };
 
 int main(){
-    Master m;
-    
+    Master m("John Doe", 1234, 5, 50000);
+    m.display();
+    m.update(6, 55000);
+    m.display();
+    return 0;
 }
