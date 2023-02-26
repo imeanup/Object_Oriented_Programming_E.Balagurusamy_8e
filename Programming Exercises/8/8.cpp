ce11a3469faed4a23e051c559c96c5aa744fe6aa
anupam <tah.anupam@gmail.com>
2023-02-26 15:05:50 +0000

    26 Feb 2023, 20:35

--- Programming Exercises/8/8.cpp
+++ Programming Exercises/8/8.cpp
@@ -8,98 +8,32 @@
 
 using namespace std;
 
-// Base class Product
+// TODO: Product: This is the base class for all the products. 
+// It contains the common attributes of all the products, such as the product ID, name, *description, price, and quantity.
 class Product {
-protected:
-    string name;
-    int quantity;
-public:
-    Product(string n, int q) : name(n), quantity(q) {}
-    virtual void display() = 0;
-    virtual string getCategory() = 0;
-    void setName(string n) { name = n; }
-    void setQuantity(int q) { quantity = q; }
-    string getName() { return name; }
-    int getQuantity() { return quantity; }
-};
+    protected:
+        int id;
+        string name;
+        int quantity;
+        double price;
 
-// Derived classes for different categories of products
-class Electronics : public Product {
-private:
-    string brand;
-    double price;
-public:
-    Electronics(string n, int q, string b, double p) : Product(n, q), brand(b), price(p) {}
-    void display() {
-        cout << "Product: " << name << endl;
-        cout << "Category: Electronics" << endl;
-        cout << "Brand: " << brand << endl;
-        cout << "Price: " << price << endl;
-        cout << "Quantity: " << quantity << endl;
-    }
-    string getCategory() { return "Electronics"; }
-};
-
-class Food : public Product {
-private:
-    string type;
-    string expiryDate;
-public:
-    Food(string n, int q, string t, string e) : Product(n, q), type(t), expiryDate(e) {}
-    void display() {
-        cout << "Product: " << name << endl;
-        cout << "Category: Food" << endl;
-        cout << "Type: " << type << endl;
-        cout << "Expiry Date: " << expiryDate << endl;
-        cout << "Quantity: " << quantity << endl;
-    }
-    string getCategory() { return "Food"; }
-};
-
-class Clothing : public Product {
-private:
-    string size;
-    string color;
-public:
-    Clothing(string n, int q, string s, string c) : Product(n, q), size(s), color(c) {}
-    void display() {
-        cout << "Product: " << name << endl;
-        cout << "Category: Clothing" << endl;
-        cout << "Size: " << size << endl;
-        cout << "Color: " << color << endl;
-        cout << "Quantity: " << quantity << endl;
-    }
-    string getCategory() { return "Clothing"; }
-};
-
-class MilkProduct : public Product {
-private:
-    string brand;
-    string expiryDate;
-public:
-    MilkProduct(string n, int q, string b, string e) : Product(n, q), brand(b), expiryDate(e) {}
-    void display() {
-        cout << "Product: " << name << endl;
-        cout << "Category: Milk Product" << endl;
-        cout << "Brand: " << brand << endl;
-        cout << "Expiry Date: " << expiryDate << endl;
-        cout << "Quantity: " << quantity << endl;
-    }
-    string getCategory() { return "Milk Product"; }
-};
-
-class KitchenProduct : public Product {
-private:
-    string brand;
-    string material;
-public:
-    KitchenProduct(string n, int q, string b, string m) : Product(n, q), brand(b), material(m) {}
-    void display() {
-        cout << "Product: " << name << endl;
-        cout << "Category: Kitchen Product" << endl;
-        cout << "Brand: " << brand << endl;
-        cout << "Material: " << material << endl;
-        cout << "Quantity: " << quantity << endl;
-    }
-    string getCategory() { return "Kitchen Product"; }
+    public:
+        Product(int id, string name, double price, int quantity) : name(name), id(id), price(price), quantity(quantity) {}
+        virtual ~Product();
+        virtual string getCategory() const = 0;
+        string getName() const { 
+            return name; 
+            }
+        int getId() const {
+            return id;
+            }
+        int getQuantity() const { 
+            return quantity; 
+            }
+        void setPrice(double price) { 
+            this->price = price; 
+            }
+        void setQuantity(int quantity) { 
+            this->quantity = quantity; 
+            }
 };
