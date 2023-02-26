e49e46aac7afdc81783b29de036ebc73144c503f
anupam <tah.anupam@gmail.com>
2023-02-26 06:20:19 +0000

    26 Feb 2023, 11:50

---
+++ Programming Exercises/8/3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Staff{
+    int code;
+    string name;
+    public:
+        void setCode(int c){
+            code = c;
+        }
+        void setName(string n){
+            name = n;
+        }
+        
+};
