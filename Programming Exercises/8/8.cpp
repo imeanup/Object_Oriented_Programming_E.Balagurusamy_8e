dafbcdba0e4ad30530508627bd14816db96ccdce
anupam <tah.anupam@gmail.com>
2023-02-26 11:12:09 +0000

    26 Feb 2023, 16:42

---
+++ Programming Exercises/8/8.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Product{
+    int id;
+    string name;
+    // string description;
+    double price;
+    int quantity;
+
+    public:
+    
+};
+
