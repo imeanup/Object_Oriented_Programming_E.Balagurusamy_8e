a62ffac14f7d8dbf7cb06f73ec26efe46731c20b
anupam <tah.anupam@gmail.com>
2023-02-26 09:13:56 +0000

    26 Feb 2023, 14:43

--- Programming Exercises/8/5.cpp
+++ Programming Exercises/8/5.cpp
@@ -3,7 +3,17 @@
 using namespace std;
 
 class Person{
+    string name;
+    int code;
 
+    public:
+        void getName(string n){
+            name = n;
+        }
+        void getCode(int c){
+            code = c;
+        }
+        
 };
 
 class Account{
