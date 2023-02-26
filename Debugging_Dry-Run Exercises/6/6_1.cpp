3d715cd80faf5718e378fcaf97776e650ac62062
anupam <tah.anupam@gmail.com>
2023-02-26 19:07:06 +0000

    27 Feb 2023, 00:37

---
+++ Debugging_Dry-Run Exercises/6/6_1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+class cube{
+    private:
+        int length;
+        int breadth;
+        int width;
+
+    public:
+        cube(): length(5), breadth(5), width(5) {}
+        cube(int l, int b): length(l), breadth(b) {}
+        int area_of_cube(){
+            return length*breadth;
+        }
+        int volume_of_cube(){
+            return length * breadth * width;
+        }
+        void display(int temp){
+            cout << temp << endl;
+        }
+};
+
+int main()
+{
+    cube A1(4,3), B1, A2(4,4);
+    int temp;
+    cout << "Default Area " << endl;
+    temp = A1.area_of_cube();
+    A1.display(temp);
+    cout << "Default volume: " << endl;
+    A1.display(temp);
+    cout << "Area when argument is 4" << endl;
+    temp = A2.area_of_cube();
+    B1.display(temp);
+    return 0;
+}
