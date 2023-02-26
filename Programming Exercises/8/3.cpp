9bc3741846fd4a399f1da5c527edc735e1ff1db3
anupam <tah.anupam@gmail.com>
2023-02-26 06:30:22 +0000

    26 Feb 2023, 12:00

--- Programming Exercises/8/3.cpp
+++ Programming Exercises/8/3.cpp
@@ -13,5 +13,72 @@ class Staff{
         void setName(string n){
             name = n;
         }
-        
+        int getCode(){
+            return code;
+        }
+        string getName(){
+            return name;
+        }
 };
+
+class Teacher : public Staff{
+    string subject;
+    string publication;
+
+    public:
+        void setSubject(string s){
+            subject = s;
+        }
+        void setPublication(string s){
+            publication = s;
+        }
+        string getSubject(){
+            return subject;
+        }
+        string getPublication(){
+            return publication;
+        }
+};
+
+class Typist : public Staff{
+    int speed;
+    public:
+        void setSpeed(int s){
+            speed = s;
+        }
+        int getSpeed(){
+            return speed;
+        }
+};
+
+class Officer : public Staff{
+    string grade;
+    public:
+        void setGrade(string s){
+            grade = s;
+        }
+        string getGrade(){
+            return grade;
+        }
+};
+
+class Regular : public Typist{
+    public:
+    //  No additional member variables or member functions
+};
+
+class Casual : public Typist{
+    float dailyWages;
+    public:
+        void setDailyWages(float w){
+            dailyWages = w;
+        }
+        float getDailyWages(){
+            return dailyWages;
+        }
+};
+
+int main(){
+    Teacher T;
+    
+}
