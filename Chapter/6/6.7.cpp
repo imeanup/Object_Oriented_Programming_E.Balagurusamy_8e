c9471df59943ba712620b09f49ceaa98202df240
anupam <tah.anupam@gmail.com>
2023-02-27 23:56:53 +0530

    6.6

--- Chapter/6/6.7.cpp
+++ Chapter/6/6.7.cpp
@@ -30,3 +30,29 @@ int main(){
     cout << "\n\nBlock inside the main block..";
     return 0;
 }
+
+/*
+Inside the main block..
+
+Creating first object T1..
+
+Constructructor Msg: Object number 1 created ..
+
+Inside Block 1..
+
+Creating two more objects T2 and T3..
+
+Constructructor Msg: Object number 2 created ..
+
+Constructructor Msg: Object number 3 created ..
+
+Leaving Block 1..
+
+Destructor Msg: Object number
+
+Destructor Msg: Object number
+
+Block inside the main block..
+
+Destructor Msg: Object number%   
+*/
