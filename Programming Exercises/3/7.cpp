f5a08643636bbbb81d724c98b30a1d08378d7f15
anupam <tah.anupam@gmail.com>
2023-02-28 11:24:33 +0000

    28 Feb 2023, 16:54

--- Programming Exercises/3/7.cpp
+++ Programming Exercises/3/7.cpp
@@ -1,3 +1,12 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
+int main(){
+    double x, term, sum = 0.0;
+    int n = 0;
+
+    cout << "Enter the value of x in radians: ";
+    cin >> x;
+    
+}
