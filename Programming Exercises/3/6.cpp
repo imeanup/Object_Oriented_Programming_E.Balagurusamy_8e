d747a0c9c59b23160c2cdd42c87b2f7f5510ea1f
anupam <tah.anupam@gmail.com>
2023-02-28 11:19:09 +0000

    28 Feb 2023, 16:49

--- Programming Exercises/3/6.cpp
+++ Programming Exercises/3/6.cpp
@@ -10,11 +10,17 @@ class Player
         int runs;
         int innings;
         int not_out;
-        float average;
+        // float average;
+        float average(int runs, int innings, int notOut) {
+            return (float)runs / (innings - notOut);
+        }
 
-        
+        void table(){
+            cout << setw(10) << "Name" << setw(10) << "Runs" << setw(10) << "Innings" << setw(10) << "Not Out" << setw(10) << "Average" << endl;
+        }
+
         void display(){
-            cout << setw(10) << name << setw(10) << runs << setw(10) << innings << setw(10) << not_out << setw(10) << average << endl;
+            cout << setw(10) << name << setw(10) << runs << setw(10) << innings << setw(10) << not_out << setw(10) << average(runs, innings, not_out) << endl;
         }
 };
  
@@ -22,19 +28,10 @@ class Player
     Player sachin = {"Sachin", 8430, 230, 18};
     Player saurav = {"Saurav", 4200, 130, 9};
     Player rahul = {"Rahul", 3350, 105, 11};
-
-    // Calculate batting averages
-    sachin.average = (float)sachin.runs / (sachin.innings - sachin.not_out);
-    saurav.average = (float)saurav.runs / (saurav.innings - saurav.not_out);
-    rahul.average = (float)rahul.runs / (rahul.innings - rahul.not_out);
-
-    // Output table header
-    cout << setw(10) << "Name" << setw(10) << "Runs" << setw(10) << "Innings" << setw(10) << "Not Out" << setw(10) << "Average" << endl;
-
-    // Output table rows
-    cout << setw(10) << sachin.name << setw(10) << sachin.runs << setw(10) << sachin.innings << setw(10) << sachin.not_out << setw(10) << sachin.average << endl;
-    cout << setw(10) << saurav.name << setw(10) << saurav.runs << setw(10) << saurav.innings << setw(10) << saurav.not_out << setw(10) << saurav.average << endl;
-    cout << setw(10) << rahul.name << setw(10) << rahul.runs << setw(10) << rahul.innings << setw(10) << rahul.not_out << setw(10) << rahul.average << endl;
+    sachin.table();
+    sachin.display();
+    saurav.display();
+    rahul.display();
 
     return 0;
 }
