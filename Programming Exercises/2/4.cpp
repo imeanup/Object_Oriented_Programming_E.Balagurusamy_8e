b3d1f3b73545b7847dad9a400f3fc1032a427a1d
anupam <tah.anupam@gmail.com>
2023-02-28 13:41:59 +0000

    28 Feb 2023, 19:11

--- Programming Exercises/2/4.cpp
+++ Programming Exercises/2/4.cpp
@@ -12,3 +12,10 @@ int main(){
     cout << "The value of x: " << x << endl;
     return 0;
 }
+
+/*
+Enter the value of a b c: 250 85 25
+The value of x: -22.0588
+Enter the value of a b c: 300 70 70
+The value of x: -65.7143
+*/
