9b15dc5f8a4703b12096b190f1c89485e40d6791
anupam <tah.anupam@gmail.com>
2023-02-28 13:28:25 +0000

    28 Feb 2023, 18:58

---
+++ Programming Exercises/2/1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int maths = 90, physics = 77, chemistry = 69;
+
+    cout << "Maths = " << maths << " Physics = " << physics << " Chemistry = " << chemistry << endl;
+
+    return 0;
+}
