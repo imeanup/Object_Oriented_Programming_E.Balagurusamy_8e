15efc8b21a803bb9e8f9973165781939c128a0ef
anupam <tah.anupam@gmail.com>
2023-02-28 13:40:52 +0000

    28 Feb 2023, 19:10

--- Programming Exercises/2/4.cpp
+++ Programming Exercises/2/4.cpp
@@ -2,5 +2,13 @@
 using namespace std;
 
 int main(){
-    int a, b, c, x;
+    float a, b, c;
+    float x;
+    cout << "Enter the value of a b c: ";
+    cin >> a >> b >> c;
+
+    x = (a/b) - c;
+
+    cout << "The value of x: " << x << endl;
+    return 0;
 }
