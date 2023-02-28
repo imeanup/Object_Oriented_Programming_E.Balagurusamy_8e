71b27d2f9bfc2c2128fd8b886b543278230c6b93
anupam <tah.anupam@gmail.com>
2023-02-28 10:32:13 +0000

    28 Feb 2023, 16:02

--- Chapter/9/1.cpp
+++ Chapter/9/1.cpp
@@ -4,6 +4,14 @@ using namespace std;
 int main(){
     int a, *ptr1, **ptr2;
     ptr1 = &a;
+    ptr2 = &ptr1;
+    cout << "a: " << ptr1 << endl;
+    cout << "ptr1: " << ptr2 << endl;
+    ptr1 += 2;
+    cout << "After incrementing the address values: " << ptr1 << endl;
+    ptr2 += 2;
+    cout << "Address of a: " << ptr1 << endl;
+    cout << "Address of ptr1: " << ptr2 << endl;
 
     return 0;
 }
