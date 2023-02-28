7b45234996d7c3a6eb302dec1dd98850267e2bc6
anupam <tah.anupam@gmail.com>
2023-02-28 13:32:28 +0000

    28 Feb 2023, 19:02

---
+++ Programming Exercises/2/2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int compare(int a, int b)
+{
+    if (a > b){
+        return a;
+    }
+    else{
+        return b;
+    }
+}
+int main(){
+    int a, b, ans;
+    cout << "Enter two number ";
+    cin >> a >> b;
+    cout << "Larger number is " << compare(a,b) << endl;
+    return 0;
+}
