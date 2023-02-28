a4007a3d4824d2718199e7aed3489d8b04622145
anupam <tah.anupam@gmail.com>
2023-02-28 17:45:03 +0530

    Reference from Page 55

--- Programming Exercises/3/8.cpp
+++ Programming Exercises/3/8.cpp
@@ -10,15 +10,17 @@ int main()
     cout << "Table for Y = EXP[-X]\n\n";
     cout << "X\t";
     for (x = 0.0; x <= 1.0; x += 0.1) {
-        cout << setw(10) << x << "\t";
+        cout << fixed << setprecision(1) << x << "\t"; 
+        // Page 55, we may also control the pecision of floating point numbers appearing
+        // in the output by using the fixed and setprecision() manipulator.
     }
     cout << endl;
 
     for (x = 0.0; x <= 9.0; x++) {
-        cout << setw(10) << x << "\t";
+        cout << fixed << setprecision(1) << x << "\t";
         for (double i = 0.0; i <= 1.0; i += 0.1) {
             y = exp(-x);
-            cout << setw(10) << y << "\t";
+            cout << fixed << setprecision(5) << y << "\t";
         }
         cout << endl;
     }
