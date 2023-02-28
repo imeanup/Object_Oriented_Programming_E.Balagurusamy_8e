8cf770e984775e538060425297fcd2d0078bb1d9
anupam <tah.anupam@gmail.com>
2023-02-28 13:37:41 +0000

    28 Feb 2023, 19:07

--- Programming Exercises/2/3.cpp
+++ Programming Exercises/2/3.cpp
@@ -2,5 +2,9 @@
 using namespace std;
 
 int main(){
-    
+    char ch;
+    cout << "Enter a character: ";
+    cin >> ch;
+    cout << "The ASCII value of " << ch << " is " << int(ch) << endl;
+    return 0;
 }
