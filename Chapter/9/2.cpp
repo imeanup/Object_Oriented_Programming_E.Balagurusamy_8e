8ba9261e29097fb1a02581c4c829251645173a12
anupam <tah.anupam@gmail.com>
2023-03-03 06:29:26 +0000

    3 Mar 2023, 11:59

--- Chapter/9/2.cpp
+++ Chapter/9/2.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(){
     int a = 10;
     int *ptr;
-    ptr = &a;
+    // ptr = &a;
     cout << "The value of a is: " << *ptr << endl;
     *ptr = *ptr + a;
     cout << "The revised value of a is: " << *ptr << endl;
