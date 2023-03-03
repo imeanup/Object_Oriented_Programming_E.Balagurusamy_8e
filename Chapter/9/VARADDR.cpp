5b28b7cd39eaae606ee37729a2a27be5aac5837b
anupam <tah.anupam@gmail.com>
2023-03-03 15:11:11 +0000

    3 Mar 2023, 20:41

---
+++ Chapter/9/VARADDR.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int var1 = 11;
+    int var2 = 22;
+    int var3 = 33;
+
+    cout << var1 << endl
+        << var2 << endl 
+        << var3 << endl;
+
+    return 0;
+}
