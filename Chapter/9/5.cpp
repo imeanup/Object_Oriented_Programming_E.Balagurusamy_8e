4d0001905da6c517c49fb844a4ce7a5fe896078c
anupam <tah.anupam@gmail.com>
2023-03-03 06:56:33 +0000

    3 Mar 2023, 12:26

---
+++ Chapter/9/5.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main(){
+    int i = 0; 
+    char *ptr[10] = {
+        "books", "tv", "computer", "sports"
+    };
+    char str[25];
+    cout << "\n\n\n\nEnter your favorite leisure pursuit: ";
+    cin >> str;
+    for (i = 0; i < 4; i++){
+        if (!strcmp(str, ptr[i])){
+            cout << "\n\nYour favourite pursuit " << "is available here" << endl;
+            break;
+        }
+    }
+    if (i == 4){
+        cout << "\n\nYour favorite " << "not available here" << endl;
+    }
+    return 0;
+}
