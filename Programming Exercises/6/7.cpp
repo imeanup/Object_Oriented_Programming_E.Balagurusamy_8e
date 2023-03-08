e58470de38a2d797461f991ab44212b19aafbe43
anupam <tah.anupam@gmail.com>
2023-03-08 19:21:40 +0000

    9 Mar 2023, 00:51

--- Programming Exercises/6/7.cpp
+++ Programming Exercises/6/7.cpp
@@ -9,6 +9,47 @@ class Cars{
         int year;
         int mileage;
     public:
-        Cars();
+        Cars(){
+            brand = "";
+            model = "";
+            year = 0;
+            mileage = 0;
+        }
+
+        Cars(string maker, string model_){
+            brand = maker;
+            model = model_;
+            year = 0;
+            mileage = 0;
+        }
+
+        Cars(string maker, string model_, int year_, int mileage_){
+            brand = maker;
+            model = model_;
+            year = year_;
+            mileage = mileage_;
+        }
+
+        void display();
 
 };
+
+void Cars :: display(){
+    cout << "Brand: " << brand << endl;
+    cout << "Model: " << model << endl;
+    cout << "Year: " << year << endl;
+    cout << "Mileage: " << mileage << endl;
+}
+
+int main(){
+    Cars car1;
+    Cars car2("Toyota", "Corolla");
+    Cars car3("Honda", "Accord", 2015, 50000);
+    
+    
+    car1.display();
+    car2.display();
+    car3.display();
+    
+    return 0;
+}
