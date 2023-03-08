7ef3ec2a316a1ae024c87e392d27c4095aa1efd3
anupam <tah.anupam@gmail.com>
2023-03-08 19:16:14 +0000

    9 Mar 2023, 00:46

--- Programming Exercises/6/7.cpp
+++ Programming Exercises/6/7.cpp
@@ -1,1 +1,14 @@
 #include <iostream>
+#include <string>
+using namespace std;
+
+class Cars{
+    private:
+        string brand;
+        string model;
+        int year;
+        int mileage;
+    public:
+        Cars();
+
+};
