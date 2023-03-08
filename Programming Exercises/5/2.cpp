4a10a7ea8f593e9df2a1baba68fcbd267dbebc08
anupam <tah.anupam@gmail.com>
2023-03-08 19:53:55 +0000

    9 Mar 2023, 01:23

--- Programming Exercises/5/2.cpp
+++ Programming Exercises/5/2.cpp
@@ -7,12 +7,12 @@ class Vector{
     private:
         vector<float> elements;
     public:
-        Vector(initializer_list<float> elems){
+        Vector(vector<float> elems){
             elements = elems;
         }
 
-        void setElement(int index, float value){
-            elements[index] = value;
+        void setElements(int index, float values){
+            elements[index] = values;
         }
 
         void scalarMultiply(float scalar){
@@ -34,12 +34,11 @@ class Vector{
 };
 
 int main(){
-    // vector<float> elements = {10.0, 20.0, 30.0};
-    // Vector v(elements);
-    Vector v = {10.0, 20.0, 30.0};
+    vector<float> elements = {10.5, 20.4, 30.7};
+    Vector v(elements);
     v.display();
 
-    v.setElement(1, 9.8);
+    v.setElements(1, 9.8);
     v.display();
 
     v.scalarMultiply(2.1);
@@ -47,7 +46,6 @@ int main(){
 
     return 0;
 }
-
 /*
 Write a class to represent a vector (a series of float values). Include member functions to perform the following tasks:
 (a) To create the vector.
@@ -55,4 +53,11 @@ Write a class to represent a vector (a series of float values). Include member f
 (c) To multiply by a scalar value
 (d) To display the vector in the form (10, 20, 30, ...)
 Write a program to test your class.
+
+compling instruction using std c++ 11:
+g++ -std=c++11 2.cpp -o 2
+./2
+(10.5, 20.4, 30.7)
+(10.5, 9.8, 30.7)
+(22.05, 20.58, 64.47)
 */
