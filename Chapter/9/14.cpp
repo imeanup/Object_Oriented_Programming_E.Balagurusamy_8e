1143f1dd4cf7a832efc88b663bde9c75477adfda
anupam <tah.anupam@gmail.com>
2023-03-09 18:24:01 +0000

    9 Mar 2023, 23:54

--- Chapter/9/14.cpp
+++ Chapter/9/14.cpp
@@ -6,3 +6,36 @@ class Exforsys{
         virtual void example() = 0;
 };
 
+class Exf1:public Exforsys{
+    public:
+        void example(){
+            cout << "C text book written by Balagurusamy" << endl;
+        }
+};
+
+class Exf2:public Exforsys{
+    public:
+        void example(){
+            cout << "C++ text Book written by Balagurusamy" << endl;
+        }
+};
+
+int main(){
+    Exforsys* arra[2];
+    Exf1 e1;
+    Exf2 e2;
+    arra[0] = &e1;
+    arra[1] = &e2;
+    arra[0]->example();
+    arra[1]->example();
+    return 0;
+}
+
+/*
+Src: https://www.exforsys.com/tutorials/c-plus-plus/c-pure-virtual-function-and-base-class.html
+The class Balagurusamy is the Base class. Inside main function, the class pointer *arra was decalred with a class name
+ "Exforsys" which was not the name of the base class.
+Page 271, Program-9.14
+
+PS: It is sad to see that even after copying, the copier copied incorrectly.
+*/
