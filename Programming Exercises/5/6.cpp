f34c7604f899dbee44d6fdd182ffd30aaeab1dc8
anupam <tah.anupam@gmail.com>
2023-03-09 04:34:15 +0000

    9 Mar 2023, 10:04

---
+++ Programming Exercises/5/6.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Matrix{
+    private:
+        int row, col;
+        vector<vector<int>> m;
+
+    public:
+        Matrix(){
+            row = 3;
+            col = 3;
+        }
+
+        Matrix(int r, int c){
+            row = r;
+            col = c;
+            m.resize(row, vector<int>(col));
+        }
+
+        void read(){
+            cout << "Enter the elements of the matrix:\n";
+            for (int i = 0; i < row; i++) {
+                for (int j = 0; j < col; j++) {
+                    cout << "m[" << i << "][" << j << "] = ";
+                    cin >> m[i][j];
+                }
+            }
+        }
+
+        void display(){
+            for (int i = 0; i < row; i++) {
+                for (int j = 0; j < col; j++) {
+                    cout << m[i][j] << " ";
+                }
+                cout << endl;
+            }
+        }
+
+        Matrix operator*(Matrix& other){
+            Matrix result(row, other.col);
+            for (int i = 0; i < row; i++) {
+                for (int j = 0; j < other.col; j++) {
+                    result.m[i][j] = 0;
+                    for (int k = 0; k < col; k++) {
+                        result.m[i][j] += m[i][k] * other.m[k][j];
+                    }
+                }
+            }
+            return result;
+        }
+};
+
+int main(){
+
+}
