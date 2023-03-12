9897ad333c77e33122c9b33f620ec3976287421e
anupam <tah.anupam@gmail.com>
2023-03-12 15:22:03 +0000

    12 Mar 2023, 20:52

---
+++ Debugging_Dry-Run Exercises/9/9,2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int ar[6] = {22, 5, 23, 43, 54, 65};
+    int *p, i;
+    p = ar;
+    
+    i = *p++ - *p--;
+    cout << i << endl;
+    return 0;
+}
