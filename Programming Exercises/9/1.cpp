b9d17f3b8f8aa615c4afe2430a939d0ed6efb732
anupam <tah.anupam@gmail.com>
2023-03-12 16:18:03 +0000

    12 Mar 2023, 21:48

--- Programming Exercises/9/1.cpp
+++ Programming Exercises/9/1.cpp
@@ -1,3 +1,52 @@
 #include <iostream>
 using namespace std;
 
+class Shape{
+    protected:
+        double x, y;
+    public:
+        void get_data(){
+            cout << "Enter the value of x and y (i.e. x y): ";
+            cin >> x >> y;
+        }
+        virtual void display_area(){
+
+        }
+};
+
+class Triangle : public Shape{
+    public:
+        void display_are(){
+            cout << "Area of triangle: " << (0.5 * x * y) << endl;
+        }
+};
+
+class Rectangle: public Shape{
+    public:
+        void display_area(){
+            cout << "Area of rectangle: " << (x*y) << endl;
+        }
+};
+
+int main(){
+    Shape *shape;
+    char choice;
+    do {
+        cout << "Enter 'r' for rectangle, 't' for triangle: ";
+        cin >> choice;
+        if (choice == 'r') {
+            shape = new Rectangle;
+        } else if (choice == 't') {
+            shape = new Triangle;
+        } else {
+            cout << "Invalid choice." << endl;
+            continue;
+        }
+        shape->get_data();
+        shape->display_area();
+        delete shape;
+        cout << "Enter 'y' to continue, any other key to exit: ";
+        cin >> choice;
+    } while (choice == 'y');
+    return 0;
+}
