ca8b4b8db0aac3b5539de047b881de001c6aee8b
anupam <tah.anupam@gmail.com>
2023-03-12 16:37:16 +0000

    12 Mar 2023, 22:07

--- Programming Exercises/9/3.cpp
+++ Programming Exercises/9/3.cpp
@@ -54,3 +54,24 @@ int main() {
     ptr->display_area();
     return 0;
 }
+
+
+/*
+a).
+If we remove the definition of display_area() from one of the derived classes, 
+then the program will still compile and run, but the derived class without 
+the display_area() definition will inherit the definition of display_area() 
+from the base class. Therefore, when we create an object of the derived class 
+without the display_area() definition and call its display_area() function, 
+the base class version of display_area() will be called, which will result in 
+incorrect output if the derived class represents a different shape with a different 
+formula for computing area.
+
+b)
+If we declare the display_area() function as virtual in the base class shape, 
+then when we create an object of a derived class and call its display_area() function, 
+the correct version of display_area() will be called based on the type of the object, 
+even if the function is not redefined in the derived class. This ensures that 
+the correct formula for computing the area of the shape represented by the object is used, 
+even if the object is passed as a pointer or reference to a function that expects a shape object.
+*/
