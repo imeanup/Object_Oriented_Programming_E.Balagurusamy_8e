dd1c5b9d61301e648bce6a98569c9e8fba2adac6
anupam <tah.anupam@gmail.com>
2023-03-12 22:37:15 +0530

    Complete PE 9

--- Programming Exercises/9/6.cpp
+++ Programming Exercises/9/6.cpp
@@ -1,3 +1,36 @@
 #include <iostream>
 using namespace std;
 
+int factorial(int n) {
+    if (n == 0 || n == 1) {
+        return 1;
+    }
+    else {
+        return n * factorial(n-1);
+    }
+}
+
+int main() {
+    int n;
+    int (*p)(int); 
+
+    cout << "Enter a number: ";
+    cin >> n;
+
+    p = &factorial; 
+
+    cout << "Factorial of " << n << " is: " << (*p)(n) << endl; 
+
+    return 0;
+}
+
+/*
+Enter a number: 4
+Factorial of 4 is: 24
+
+Enter a number: 6
+Factorial of 6 is: 720
+
+Enter a number: 7
+Factorial of 7 is: 5040
+*/
