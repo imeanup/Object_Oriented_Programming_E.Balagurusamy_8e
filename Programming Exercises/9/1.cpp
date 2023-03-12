6af48c4055a4c18f1d0f3f8a9d5fb257f204d84e
anupam <tah.anupam@gmail.com>
2023-03-12 14:52:19 +0000

    12 Mar 2023, 20:22

--- Programming Exercises/9/1.cpp
+++ Programming Exercises/9/1.cpp
@@ -1,19 +1,3 @@
 #include <iostream>
 using namespace std;
 
-// class 1
-// {
-// private:
-//     /* data */
-// public:
-//     1(/* args */);
-//     ~1();
-// };
-
-// 1::1(/* args */)
-// {
-// }
-
-// 1::~1()
-// {
-// }
