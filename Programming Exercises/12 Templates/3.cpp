d4c10a5d0b2cc0120bd7107cd053cb1acabdb12b
anupam <tah.anupam@gmail.com>
2023-03-14 08:13:35 +0000

    14 Mar 2023, 13:43

---
+++ Programming Exercises/12 Templates/3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+template <class T> void linearSort(T v[], int n){
+    for (int i = 0; i < n-1; i++){
+        for (int j = i; j < n; j++){
+            if (a[j] < a[])
+        }
+    }
+}
