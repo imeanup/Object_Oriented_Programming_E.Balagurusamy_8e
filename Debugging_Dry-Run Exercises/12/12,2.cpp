67bd1d20f301a3d5332a4bb7a2045fef8c35a318
anupam <tah.anupam@gmail.com>
2023-03-14 04:51:37 +0000

    14 Mar 2023, 10:21

---
+++ Debugging_Dry-Run Exercises/12/12,2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+template<class T = int> void fun(T a, int b){
+    cout << "Fun1: " << a << " " << b << endl;
+}
+
+template<class T1, class T2> void fun(T1 a, T2 b){
+    cout << "Fun2: " << a << " " << b << endl;
+}
+
+void fun(int a = 30, int b = 35){
+    cout << "Fun3: " << a << " " << b << endl;
+}
+
+int main(){
+    fun(25, 28);
+    fun(25, 28.0);
+    fun(25.0, 28);
+    fun();
+    fun(45);
+    return 0;
+}
