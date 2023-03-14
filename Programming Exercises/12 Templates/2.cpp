1af66d753d5816f3ed7ac9ec45ba73dc4988d50b
anupam <tah.anupam@gmail.com>
2023-03-14 06:57:38 +0000

    14 Mar 2023, 12:27

--- Programming Exercises/12 Templates/2.cpp
+++ Programming Exercises/12 Templates/2.cpp
@@ -1,3 +1,9 @@
+// Write a class template to represent a generic vector. Include member functions to perform the following tasks:
+// a) To create the vector
+// b) To modify the value of a given element.
+// c) To multiply by a scalar value.
+// d) To display the vector in the form (10, 20, 30,....).
+
 #include <iostream>
 using namespace std;
 
