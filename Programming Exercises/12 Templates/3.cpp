d2306dcf3df09f309773dfcce75193ae6931a15e
anupam <tah.anupam@gmail.com>
2023-03-14 08:25:39 +0000

    14 Mar 2023, 13:55

--- Programming Exercises/12 Templates/3.cpp
+++ Programming Exercises/12 Templates/3.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
 using namespace std;
 
-template <class T> void linearSort(T v[], int n){
-    for (int i = 0; i < n-1; i++){
-        for (int j = i; j < n; j++){
-            if (a[j] < a[])
+template <class T> T linearSearch(T arr[], int n, T key){
+    int i;
+    while (i < n){
+        if (arr[i] == key){
+            cout << "Element at index: " << i << endl;
         }
+        i++;
     }
+    cout << "Element not present" << endl;
+}
+
+int main(){
+    int arr[] = { 1, 9, 42, 0, -2, -42, 42, 5 };
+    int size = sizeof(arr)/sizeof(int);
+    int key = 42;
+
+    linearSearch(arr, size, key);
 }
