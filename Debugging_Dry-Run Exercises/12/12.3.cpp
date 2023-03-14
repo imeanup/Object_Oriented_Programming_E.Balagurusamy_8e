95053cb43c702911c73c6ea879f02d25053d5323
anupam <tah.anupam@gmail.com>
2023-03-14 04:12:43 +0000

    14 Mar 2023, 09:42

--- Debugging_Dry-Run Exercises/12/12.3.cpp
+++ Debugging_Dry-Run Exercises/12/12.3.cpp
@@ -1,12 +1,18 @@
 #include <iostream> 
 using namespace std;
 
-template <class T1, class T2> T1 &MinMax(T1 t1, T2 t2){
-    return t1 > t2 ? t1:t2;
+template <class T1, class T2> T1 MinMax(T1 t1, T2 t2){
+    return t1 > t2 ? t1 : t2;
     cout << " ";
 }; 
 
 int main(){
-    cout << ++MinMax(2, 3);
+    int result;
+    result = MinMax(2, 3);
+    cout << ++result << endl;
     return 0;
 }
+
+/*
+4
+*/
