2fbe26b3a8f17ed71f56d50be9704298757f59b2
anupam <tah.anupam@gmail.com>
2023-03-16 07:51:10 +0000

    16 Mar 2023, 13:21

--- Chapter/10/1.cpp
+++ Chapter/10/1.cpp
@@ -1,15 +1,11 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
 int main(){
     int count = 0;
     char c;
-    string s;
     cout << "INPUT TEXT\n";
     cin.get(c);
-    cin >> s;
-    cout << sizeof(s) << endl;
     while (c != '\n'){
         cout.put(c);
         count++;
