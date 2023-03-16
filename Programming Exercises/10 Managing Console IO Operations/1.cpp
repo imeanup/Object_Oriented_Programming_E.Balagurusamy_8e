86dab0c33864d77fd22bc86f37fe226c35b9333b
anupam <tah.anupam@gmail.com>
2023-03-16 08:45:32 +0000

    16 Mar 2023, 14:15

--- Programming Exercises/10 Managing Console IO Operations/1.cpp
+++ Programming Exercises/10 Managing Console IO Operations/1.cpp
@@ -1,2 +1,42 @@
 #include <iostream>
+#include <vector>
+#include <iomanip>
+#include <string>
 using namespace std;
+
+int main(){
+    vector<string> names;
+    vector<int> codes;
+    vector<double> costs;
+
+    cout << "Enter item details (name, code, cost): " << endl;
+    string name;
+    int code;
+    double cost;
+
+    while (true){
+        cout << "Name (or empty to stop): ";
+        getline(cin, name);
+
+        if (name.empty()){
+            break;
+        }
+
+        cout << "Code: ";
+        cin >> code;
+        cout << "Cost: ";
+        cin >> cost;
+
+        names.push_back(name);
+        codes.push_back(code);
+        costs.push_back(cost);
+
+        cin.ignore();
+    }
+    cout << left << setw(10) << "NAME" << setw(10) << "CODE" << setw(10) << "COST" << endl;
+
+    for (int i = 0; i < names.size(); i++){
+        cout << left << setw(10) << names[i] << setw(10) << codes[i] << setw(10) << costs[i] << endl;
+    }
+    return 0;
+}
