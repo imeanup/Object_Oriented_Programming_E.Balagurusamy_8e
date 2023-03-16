1397d86a99a4e80c6b87a044625f5ffb500ae1cd
anupam <tah.anupam@gmail.com>
2023-03-16 09:44:01 +0000

    16 Mar 2023, 15:14

--- Programming Exercises/10 Managing Console IO Operations/4.cpp
+++ Programming Exercises/10 Managing Console IO Operations/4.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <cmath>
+
 using namespace std;
 
-int main(){
+int main() {
+    double e = 1.0;
+    double term = 1.0; 
+    int n = 1; 
+
+    while (abs(term) > 0.0001) { 
+        term = term * 1.0 / n; 
+        e += term; 
+        n++; 
+    }
+
+    cout << "e = " << e << endl;
+
     return 0;
 }
