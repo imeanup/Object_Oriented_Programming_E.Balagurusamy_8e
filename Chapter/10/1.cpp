51b8f6638a5afa1c580cbeaf45a80a67a1054be4
anupam <tah.anupam@gmail.com>
2023-03-16 07:30:54 +0000

    16 Mar 2023, 13:00

---
+++ Chapter/10/1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    int code;
+    string cha;
+    cin >> code;
+    cout << code << endl;
+    cin >> cha;
+    cout << cha << endl;
+    return 0;
+}
