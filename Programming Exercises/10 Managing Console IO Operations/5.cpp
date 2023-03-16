28fc10dde4a07de054e8750f7a29b70417316772
anupam <tah.anupam@gmail.com>
2023-03-16 09:16:28 +0000

    16 Mar 2023, 14:46

--- Programming Exercises/10 Managing Console IO Operations/5.cpp
+++ Programming Exercises/10 Managing Console IO Operations/5.cpp
@@ -2,5 +2,29 @@
 using namespace std;
 
 int main(){
+    float pi = 22.0/7.0;
+    int i, n;
+    cout << "Enter the precison size: ";
+    cin >> n;
+    cout << "Value of Pi:" << endl;
+    for (i = 1; i <= n; i++){
+        cout.precision(i);
+        cout << pi << endl;
+    }
     return 0;
 }
+
+/*
+Enter the precison size: 10
+Value of Pi:
+3
+3.1
+3.14
+3.143
+3.1429
+3.14286
+3.142857
+3.1428571
+3.14285707
+3.142857075
+*/
