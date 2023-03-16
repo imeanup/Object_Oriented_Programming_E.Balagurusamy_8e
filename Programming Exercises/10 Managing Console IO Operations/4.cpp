6a09df864e0ebefc599a6a2cacb2701cfc39b03c
anupam <tah.anupam@gmail.com>
2023-03-16 18:57:28 +0000

    17 Mar 2023, 00:27

--- Programming Exercises/10 Managing Console IO Operations/4.cpp
+++ Programming Exercises/10 Managing Console IO Operations/4.cpp
@@ -4,20 +4,24 @@
 using namespace std;
 
 int main() {
-    double e = 1.0;
-    double term = 1.0; 
-    int n = 1; 
-
-    while (abs(term) > 0.0001) { 
-        term = term * 1.0 / n; 
-        e += term; 
-        n++; 
+    double e = 1.0, term = 1.0;
+    int i = 1, n;
+    double acc = 0.0001;
+    cout << "Enter the value of n: ";
+    cin >> n;
+    while (i <= n)
+    {
+        term *= i;
+        e += 1.0 / term;
+        i++;
     }
-
-    cout << "e = " << e << endl;
-
+    cout << "The value of e is " << e << endl;
     return 0;
 }
 /*
-e = 2.71828
+Enter the value of n: 3
+The value of e is 2.66667 
+
+Enter the value of n: 4
+The value of e is 2.70833
 */
