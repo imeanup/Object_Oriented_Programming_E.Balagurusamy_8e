9708caf13cab96392346770c27bcfcd229cd2f1e
anupam <tah.anupam@gmail.com>
2023-03-16 18:50:58 +0000

    17 Mar 2023, 00:20

---
+++ Programming Exercises/10 Managing Console IO Operations/test.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+double fact(int n)
+{
+	if(n==1)
+		return 1.0;
+	else
+		return n*fact(n-1);
+}
+int main()
+{
+	double e=1.0;
+	int n,i;
+	//clrscr(); //changed
+	cout<<"Enter Limit of Series..."<<endl;
+	cin>>n;
+	for(i=1;i<=n;i++)
+	{
+		e=e+(1.0/fact(i));
+	}
+	cout.precision(4);//For Accuracy acc=0.0001
+	cout<<"e = "<<e;
+	//getch();
+}
