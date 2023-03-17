5f4b7142b64659687a26e44caa9fa9c7ec10dfae
anupam <tah.anupam@gmail.com>
2023-03-17 12:14:32 +0000

    17 Mar 2023, 17:44

---
+++ Chapter/10/2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+    int size = 30;
+    char city[20];
+    cout << "Enter the city name: ";
+    cin >> city;
+    cout << "City name: " << city << endl;
+
+    cout << "Enter city name again: ";
+    cin.getline(city, size);
+    cout << "City name now: " << city << endl;
+
+    cout << "Enter another city name: ";
+    cin.getline(city, size);
+    cout << "New city name: " << city << endl;
+    return 0;
+}
