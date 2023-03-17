2939c66242535d1368a4235505af1733cd0ed79a
anupam <tah.anupam@gmail.com>
2023-03-17 14:05:38 +0000

    17 Mar 2023, 19:35

---
+++ Chapter/10/5.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    cout << "Precision set to 3 digits" << endl;
+    cout.precision(3);
+
+    cout.width(10);
+    cout << "VALUE";
+}
