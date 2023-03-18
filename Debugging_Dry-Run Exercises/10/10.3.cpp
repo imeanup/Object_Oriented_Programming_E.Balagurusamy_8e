1ae4807a87f94371da6283289822dca97dcfb5eb
anupam <tah.anupam@gmail.com>
2023-03-18 14:06:56 +0000

    18 Mar 2023, 19:36

---
+++ Debugging_Dry-Run Exercises/10/10.3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    float pi = 22.0/7.0;
+    // clrscr();
+    cout.fill('$');
+    cout.width(10);
+    cout.precision(2);
+    cout << pi << endl;
+
+    return 0;
+}
