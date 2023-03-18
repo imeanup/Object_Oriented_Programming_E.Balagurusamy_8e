a12ba34cc54553432708738023249ebd9a146783
anupam <tah.anupam@gmail.com>
2023-03-18 16:52:29 +0000

    18 Mar 2023, 22:22

---
+++ Programming Exercises/11 Working with Files/2.cpp
@@ -0,0 +1,11 @@
+/*
+A file contains a list of telephone numbers in the following form 
+
+John    23456
+Ahmed   9876
+....    ....
+
+The names contain only one word and the names and telephone numbers are separated by white spaces. 
+Write program to read this file and output the list in two columns. 
+The names should be left justified and the numbers should be right justified.
+*/
