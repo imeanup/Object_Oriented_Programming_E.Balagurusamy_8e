30b46bb29a672a6eabb3f4b5ea80141a8c9ff9c7
anupam <tah.anupam@gmail.com>
2023-03-18 16:43:09 +0000

    18 Mar 2023, 22:13

--- Programming Exercises/11 Working with Files/1.cpp
+++ Programming Exercises/11 Working with Files/1.cpp
@@ -1,6 +1,11 @@
 // Write a program that reads a text file and creates another file that is identical except that every sequence of consecutive blank spaces is replaced by a single space.
 
-
 #include <iostream>
+#include <string>
+#include <fstream>
+
 using namespace std;
 
+int main(){
+    
+}
