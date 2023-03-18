a04e8f0e351b003ad3ff4702374d9912a5d1bc34
anupam <tah.anupam@gmail.com>
2023-03-18 08:55:53 +0000

    18 Mar 2023, 14:25

--- Chapter/10/5.cpp
+++ Chapter/10/5.cpp
@@ -9,4 +9,34 @@ int main(){
 
     cout.width(10);
     cout << "VALUE";
+    cout.width(15);
+    cout << "SQRT OF VALUE" << endl;
+
+    for (int i = 1; i <= 5; i++){
+        cout.width(8);
+        cout << i;
+        cout.width(13);
+        cout << sqrt(i) << endl;
+    }
+
+    cout << "Precision set to 5 digits" << endl;
+    cout.precision(5);
+    cout << "sqrt(10) = " << sqrt(10) << endl;
+
+    cout.precision(0);
+    cout << "sqrt(10) = " << sqrt(10) << "default setting" << endl;
+    return 0;
 }
+
+/*
+Precision set to 3 digits
+     VALUE  SQRT OF VALUE
+       1            1
+       2         1.41
+       3         1.73
+       4            2
+       5         2.24
+Precision set to 5 digits
+sqrt(10) = 3.1623
+sqrt(10) = 3default setting
+*/
