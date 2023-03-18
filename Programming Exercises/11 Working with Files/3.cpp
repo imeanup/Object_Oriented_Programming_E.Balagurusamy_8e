d1b0290d6d1ecb45f0e4b3834f2b352e674e3450
anupam <tah.anupam@gmail.com>
2023-03-18 17:08:41 +0000

    18 Mar 2023, 22:38

--- Programming Exercises/11 Working with Files/3.cpp
+++ Programming Exercises/11 Working with Files/3.cpp
@@ -2,7 +2,6 @@
 Write a program that will create a data file containing the list of telephone numbers given in exercise in 11.2. 
 Use a class object to store each set of data.
 */
-
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -13,6 +12,8 @@ class Telephone {
 public:
     string name;
     string number;
+
+    Telephone() {}
 
     Telephone(string name, string number) {
         this->name = name;
@@ -25,19 +26,44 @@ public:
 };
 
 int main() {
-    Telephone telephones[] = {
-        Telephone("John", "23456"),
-        Telephone("Ahmed", "9876"),
-        // Add more Telephone objects here
-    };
+    int n;
+    cout << "Enter the number of records to be added: ";
+    cin >> n;
+    Telephone* telephones = new Telephone[n];
+
+    int count = 0;
+
+    while (count < n) {
+        string name, number;
+        cout << "Enter name: ";
+        cin >> name;
+        cout << "Enter number: ";
+        cin >> number;
+        telephones[count] = Telephone(name, number);
+        count++;
+    }
 
     ofstream file("telephone_numbers.txt");
     if (file.is_open()) {
-        for (Telephone telephone : telephones) {
-            file << telephone.toString() << endl;
+        for (int i = 0; i < n; i++) {
+            file << telephones[i].toString() << endl;
         }
         file.close();
     }
 
+    delete[] telephones;
+
     return 0;
 }
+
+/*
+Enter the number of records to be added: 3 
+Enter name: Mike
+Enter number: 1234
+Enter name: John
+Enter number: 3456
+Enter name: Cine
+Enter number: 7890
+
+Open file name telephone_numbers.txt
+*/
---
+++ Programming Exercises/11 Working with Files/4.cpp
@@ -0,0 +1,10 @@
+/*
+Write an interactive, menu-driven program that will access the file created in exercise 11.3 and implement the tasks:
+(a) Determine the telephone number of the specified person.
+(b) Determine the name if a telephone number is known.
+(c) Update, the telephone number, whenever there is a change.
+*/
+
+#include <iostream>
+
+using namespace std;
