87c9e22b6ffcd59e1674e23808ee4f630f9826b0
anupam <tah.anupam@gmail.com>
2023-03-22 16:56:49 +0000

    22 Mar 2023, 22:26

--- Programming Exercises/13 Exception Handling/1.cpp
+++ Programming Exercises/13 Exception Handling/1.cpp
@@ -1,2 +1,20 @@
 // Write a program containing a possible exception. Use a try block to throw it and a catch block to handle it properly.
 
+#include <iostream>
+using namespace std;
+
+int main(){
+    try{
+        cout << "Inside try block" << endl;
+        throw 10;
+    }
+    catch(int e){
+        cout << "An exception occurred. Exception number: " << e << endl;
+    }
+    return 0;
+}
+
+/*
+Inside try block
+An exception occurred. Exception number: 10
+*/
