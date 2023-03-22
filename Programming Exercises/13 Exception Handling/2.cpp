11de97b48ee1996f1b33b40eea8740afa74ca8df
anupam <tah.anupam@gmail.com>
2023-03-22 16:58:57 +0000

    22 Mar 2023, 22:28

--- Programming Exercises/13 Exception Handling/2.cpp
+++ Programming Exercises/13 Exception Handling/2.cpp
@@ -1,1 +1,23 @@
 // Write a program that illustrates the application of multiple catch statements.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    try {
+        cout << "Inside try block" << endl;
+        throw 'a';
+    }
+    catch (int e) {
+        cout << "An exception occurred. Exception number: " << e << endl;
+    }
+    catch (char c) {
+        cout << "An exception occurred. Exception character: " << c << endl;
+    }
+    return 0;
+}
+
+/*
+Inside try block
+An exception occurred. Exception character: a
+*/
