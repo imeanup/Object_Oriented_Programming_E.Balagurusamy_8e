645e60764da1b53aa7ca2ee64751c386a6afd6ef
anupam <tah.anupam@gmail.com>
2023-03-22 17:55:07 +0000

    22 Mar 2023, 23:25

--- Programming Exercises/13 Exception Handling/8.cpp
+++ Programming Exercises/13 Exception Handling/8.cpp
@@ -1,1 +1,13 @@
 // Write a program to create your own exception by inheriting exception class functionality.
+
+#include <iostream>
+#include <exception>
+
+using namespace std;
+
+class MyException: public exception{
+    public:
+        const char *what() const noexcept override{
+            return "My custom exception";
+        }
+};
