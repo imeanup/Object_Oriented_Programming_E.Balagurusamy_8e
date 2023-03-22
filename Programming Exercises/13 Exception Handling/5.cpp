67a4f0550b34bfe0f14e62202b0d213cc60fefd7
anupam <tah.anupam@gmail.com>
2023-03-22 17:29:56 +0000

    22 Mar 2023, 22:59

--- Programming Exercises/13 Exception Handling/5.cpp
+++ Programming Exercises/13 Exception Handling/5.cpp
@@ -2,3 +2,29 @@
 
 #include <iostream>
 using namespace std;
+
+void handleException(){
+    try{
+        throw 20;
+    }
+    catch(int e){
+        cout << "An exception occurred in handleException(). Exception number: " << e << endl;
+        // rethrow exception.
+        throw;
+    }
+}
+
+int main(){
+    try{
+        handleException();
+    }
+    catch(int e){
+        cout << "An exception occurred in main(). Exception number: " << e << endl;
+    }
+    return 0;
+}
+
+/*
+An exception occurred in handleException(). Exception number: 20
+An exception occurred in main(). Exception number: 20
+*/
---
+++ Programming Exercises/13 Exception Handling/6.cpp
@@ -0,0 +1,8 @@
+/*
+Write a program with the following:
+a) A function to read two doublw type numbers from keyboard.
+b) A function to calculate the division of these two numbers.
+c) A try block to throw an exception when a wrong type of data is keyed in.
+d) A try block to detect and throw an exception if the condition "divide-by-zero" occurs.
+e) Appropriate catch block to handle the exception thrown.
+*/
