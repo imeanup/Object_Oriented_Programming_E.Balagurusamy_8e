dd4ce2a5e9c084c23cd9c551e8e4d79c9ec85157
anupam <tah.anupam@gmail.com>
2023-03-22 17:50:36 +0000

    22 Mar 2023, 23:20

--- Programming Exercises/13 Exception Handling/7.cpp
+++ Programming Exercises/13 Exception Handling/7.cpp
@@ -1,3 +1,46 @@
 /*
 Write a main program that calls a deeply nexted function containing an exception. Incorporate necessary exception handling mechanism.
 */
+
+#include <iostream>
+using namespace std;
+
+void levelThree();
+void levelTwo();
+void levelOne(){
+    cout << "Entering levelOne()" << endl;
+    levelTwo();
+    cout << "Exiting levelOne()" << endl;
+}
+
+void levelTwo(){
+    cout << "Entering levelTwo()" << endl;
+    levelThree();
+    cout << "Exiting levelTwo()" << endl;
+}
+
+void levelThree(){
+    cout << "Entering levelThree()" << endl;
+    throw 10;
+    cout << "Exiting levelThree()" << endl;
+}
+
+int main(){
+    try{
+        cout << "Entering main()" << endl;
+        levelOne();
+        cout << "Exiting main()" << endl;
+    }
+    catch(int e){
+        cout << "An exception occurred. Exception number: " << e << endl;
+    }
+    return 0;
+}
+
+/*
+Entering main()
+Entering levelOne()
+Entering levelTwo()
+Entering levelThree()
+An exception occurred. Exception number: 10
+*/
---
+++ Programming Exercises/13 Exception Handling/8.cpp
@@ -0,0 +1,1 @@
+// Write a program to create your own exception by inheriting exception class functionality.
