e78d06689376bb0e9a2265553db0ee013cdaedfd
anupam <tah.anupam@gmail.com>
2023-03-23 14:47:08 +0000

    23 Mar 2023, 20:17

--- Programming Exercises/14 Introduction to the Standard Template Library/4.cpp
+++ Programming Exercises/14 Introduction to the Standard Template Library/4.cpp
@@ -1,2 +1,23 @@
 // Redo the Exercise 14.17 using a set. (I believe it was Ex. 14.1)
 
+#include <iostream>
+#include <set>
+using namespace std;
+int main() {
+    set<int> s;
+    s.insert(0);
+    s.insert(9);
+    for (int i = 1; i < 9; i++) {
+        s.insert(1);
+    }
+    set<int>::iterator it;
+    for (it = s.begin(); it != s.end(); ++it) {
+        cout << *it << ' ';
+    }
+    cout << endl;
+    return 0;
+}
+
+/*
+0 1 9 
+*/
---
+++ Programming Exercises/14 Introduction to the Standard Template Library/5.cpp
@@ -0,0 +1,3 @@
+// A table gives a list of car models and the numbers of units sold in each type in a specified period. 
+// Write a program to store this table in a suitable container,
+// and to display interactively the total value of a particular model sold, given the unit-cost of that model.
