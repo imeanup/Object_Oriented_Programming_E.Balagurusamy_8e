608c59df6fb105d7c8bf16d1b02d8cf6155bd559
anupam <tah.anupam@gmail.com>
2023-03-23 13:01:01 +0000

    23 Mar 2023, 18:31

--- Programming Exercises/14 Introduction to the Standard Template Library/1.cpp
+++ Programming Exercises/14 Introduction to the Standard Template Library/1.cpp
@@ -1,1 +1,23 @@
-// TODO
+// Write a code segment that does the following:
+// a) Defines a vector v with a maximum size of 10.
+// b) Set the first element of v to 0.
+// c)Sets the last element of v to 9.
+// d) Sets the other elements to 1.
+// e) Displays the contents of v.
+
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void display(vector<int> &v){
+    for (int i = 0; i < v.size(); i++){
+        cout << v[i] << " ";
+    }
+    cout << endl;
+}
+
+int main(){
+    vector <int> v;
+}
