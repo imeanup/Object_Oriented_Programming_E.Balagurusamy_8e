b0d47bbec5f5c7e7c322b965cbcc0196d9733019
anupam <tah.anupam@gmail.com>
2023-03-23 14:23:15 +0000

    23 Mar 2023, 19:53

--- Programming Exercises/14 Introduction to the Standard Template Library/3.cpp
+++ Programming Exercises/14 Introduction to the Standard Template Library/3.cpp
@@ -21,5 +21,25 @@ class Student{
 
 int main(){
     list <Student> phone_lit = {Student("Alice", 1), Student("Bob", 2), Student("Charlie", 3), Student("Harry", 4), Student("Hermony", 5)};
+
+    int search_roll_number = 3;
+    list<Student> :: iterator it;
+    for (it = phone_lit.begin(); it != phone_lit.end(); ++it){
+        if (it->roll_number == search_roll_number){
+            cout << "Name: " << it->first_name << endl;
+            break;
+        }
+    }
+
+    string search_name = "Charlie";
+    for (it = phone_lit.begin(); it != phone_lit.end(); ++it){
+        // if (strcmp(it->first_name, search_name)){
+
+        // }
+        if (it->first_name == search_name){
+            cout << "Roll Number: " << it->roll_number << endl;
+            break;
+        }
+    }
     
 }
