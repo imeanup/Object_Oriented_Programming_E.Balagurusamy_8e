604ce965c6eb936a16c321c0bb961673279b3cb6
anupam <tah.anupam@gmail.com>
2023-03-23 13:41:02 +0000

    23 Mar 2023, 19:11

--- Programming Exercises/14 Introduction to the Standard Template Library/6.cpp
+++ Programming Exercises/14 Introduction to the Standard Template Library/6.cpp
@@ -1,3 +1,5 @@
 // Create an array with even number and a list with odd numbers. 
 // Merge two sequences of numbers into a vector using the algorithms merge().
 // Display the vector.
+
+#include <iostream>
