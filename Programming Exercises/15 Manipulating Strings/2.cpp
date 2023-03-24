052e6b08b2d8dfe6c585157bedf100297e2932bf
anupam <tah.anupam@gmail.com>
2023-03-24 17:04:34 +0000

    24 Mar 2023, 22:34

--- Programming Exercises/15 Manipulating Strings/2.cpp
+++ Programming Exercises/15 Manipulating Strings/2.cpp
@@ -1,3 +1,26 @@
 /*
 Write a program using an iterator and while() construct to display the contents of a string object.
 */
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string str = "Hello World!";
+    string::iterator it = str.begin();
+
+    while (it != str.end())
+    {
+        cout << *it;
+        it++;
+    }
+
+    return 0;
+}
+
+/*
+Hello World!
+*/
---
+++ Programming Exercises/15 Manipulating Strings/3.cpp
@@ -0,0 +1,3 @@
+/*
+Write a program that reads 
+*/
