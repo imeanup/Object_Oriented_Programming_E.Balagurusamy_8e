cf5ad23057c5ca3acbfbfd2b5a403665eb6eed54
anupam <tah.anupam@gmail.com>
2023-03-24 18:36:15 +0000

    25 Mar 2023, 00:06

--- Programming Exercises/16 New Features of ANSI C++ Standard/1.cpp
+++ Programming Exercises/16 New Features of ANSI C++ Standard/1.cpp
@@ -3,16 +3,19 @@ Write a program in which increment in pointer to integer should demonstrate incr
 */
 
 #include <iostream>
-using namespace std;
 
-int main(){
+int main() {
     int x = 10;
-    int *p = &x;
-    cout << "x = " << x << endl;
-    cout << "p = " << p << endl;
+    int* p = &x;
 
+    std::cout << "x = " << x << std::endl;
+    std::cout << "p = " << p << std::endl;
+
+    // Increment p using reinterpret_cast
     p = reinterpret_cast<int*>(reinterpret_cast<char*>(p) + sizeof(int));
-    cout << "p(after) = " << p << endl;
-    cout << "*p(after) = " << *p << endl;
+
+    std::cout << "p (after increment) = " << p << std::endl;
+    std::cout << "*p (after increment) = " << *p << std::endl;
+
     return 0;
 }
