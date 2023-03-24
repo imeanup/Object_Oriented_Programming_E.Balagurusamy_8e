c6cfbe2ff0c2a38e799ac9684340ff934a24a9e7
anupam <tah.anupam@gmail.com>
2023-03-24 17:02:32 +0000

    24 Mar 2023, 22:32

--- Programming Exercises/15 Manipulating Strings/1.cpp
+++ Programming Exercises/15 Manipulating Strings/1.cpp
@@ -6,3 +6,33 @@
  b) append() function.
 */
 
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string first_name, middle_name, last_name;
+    cout << "Enter first name: ";
+    cin >> first_name;
+    cout << "Enter middle name: ";
+    cin >> middle_name;
+    cout << "Enter last name: ";
+    cin >> last_name;
+    cout << endl;
+    string full_name = first_name + " " + middle_name + " " + last_name;
+    cout << "Full name using + operator: " << full_name << endl;
+
+    string full_name_append = first_name.append(" ").append(middle_name).append(" ").append(last_name);
+    cout << "Full name using append(): " << full_name_append << endl;
+
+    return 0;
+}
+
+/*
+Enter first name: Martin
+Enter middle name: Luther  
+Enter last name: King
+
+Full name using + operator: Martin Luther King
+Full name using append(): Martin Luther King
+*/
---
+++ Programming Exercises/15 Manipulating Strings/2.cpp
@@ -0,0 +1,3 @@
+/*
+Write a program using an iterator and while() construct to display the contents of a string object.
+*/
