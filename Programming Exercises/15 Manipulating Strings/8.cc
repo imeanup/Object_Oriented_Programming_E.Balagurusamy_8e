2395c75549565302b3b66934678d58a43831c20f
anupam <tah.anupam@gmail.com>
2023-03-24 18:17:50 +0000

    24 Mar 2023, 23:47

--- Programming Exercises/15 Manipulating Strings/8.cc
+++ Programming Exercises/15 Manipulating Strings/8.cc
@@ -11,26 +11,25 @@ write a program that displays the following:
 */
 #include <iostream>
 #include <string>
-
 using namespace std;
 
 int main() {
-    string s("abcdefghi");
+    string s("123456789");
     int n = s.size();
-    int k = 0;
-    for (int i = 1; i <= n; i += 2) {
-        for (int j = 1; j <= n - i; j += 2) {
-            cout << " ";
-        }
-        for (int j = 1; j <= i; ++j) {
-            cout << s[k];
-            ++k;
-            if (j != i) {
+    int m = 2*n - 1;
+    for (int i = 1; i <= n; i++) {
+        int k = i - 1;
+        for (int j = 1; j <= m; j++) {
+            if (j <= n - i || j >= n + i) {
                 cout << " ";
+            } else {
+                cout << s[k];
+                if (j < n) {
+                    k++;
+                } else {
+                    k--;
+                }
             }
-        }
-        for (int j = n - i + 1; j <= n; j += 2) {
-            cout << " ";
         }
         cout << endl;
     }
