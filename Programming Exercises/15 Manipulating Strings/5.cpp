c5ed60921e7d653fc8e72a141c884d0fbc93c96d
anupam <tah.anupam@gmail.com>
2023-03-24 17:24:48 +0000

    24 Mar 2023, 22:54

--- Programming Exercises/15 Manipulating Strings/5.cpp
+++ Programming Exercises/15 Manipulating Strings/5.cpp
@@ -1,3 +1,25 @@
 /*
 Write a program that counts the number of occurrences of a particular character, say 'e, in a line of text.
 */
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string text;
+    char ch;
+    int count = 0;
+    cout << "Enter the line of text: "; 
+    getline(cin, text);
+    cout << "Enter the character to be counted: ";
+    cin >> ch;
+    string :: iterator it;
+    for (it = text.begin(); it != text.end(); ++it){
+        if (*it == ch){
+            count++;
+        }
+    }
+    cout << "Number of occurrence of " << ch << " is " << count << endl;
+    return 0;
+}
