98ed0efeb0a8e38261d6b357de4573ed0da184ba
anupam <tah.anupam@gmail.com>
2023-03-24 17:38:36 +0000

    24 Mar 2023, 23:08

--- Programming Exercises/15 Manipulating Strings/7.cc
+++ Programming Exercises/15 Manipulating Strings/7.cc
@@ -4,10 +4,42 @@ Write a program that reads a list of countries in random order and displays them
 
 #include <iostream>
 #include <string>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int main(){
+bool compare(string a, string b) {
+    return a < b;
+}
 
+int main(){
+    vector <string> countries;
+    string country;
+    cout << "Enter a list of countries (type 'done' when finished): \n";
+    while (cin >> country && country != "done"){
+        countries.push_back(country);
+    }
+    sort(countries.begin(), countries.end(), compare);
+    cout << "Countries in alphabetical order:\n";
+    for (auto c : countries) {
+        cout << c << '\n';
+    }
     return 0;
 }
+
+/*
+Enter a list of countries (type 'done' when finished): 
+B
+Z
+X
+A
+P
+done
+Countries in alphabetical order:
+A
+B
+P
+X
+Z
+*/
---
+++ Programming Exercises/15 Manipulating Strings/8.cc
@@ -0,0 +1,9 @@
+/*
+Given a string
+string s("123456789");
+
+write a progrm that displays the following:
+
+
+5 6 7 8 9 8 7 6 5
+*/
