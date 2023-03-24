b302639a415fd9f5203b29472278b67675b76dd7
anupam <tah.anupam@gmail.com>
2023-03-25 00:20:04 +0530

    Complete 16

--- Programming Exercises/16 New Features of ANSI C++ Standard/3.cpp
+++ Programming Exercises/16 New Features of ANSI C++ Standard/3.cpp
@@ -1,3 +1,26 @@
 /*
 Write a program to demonstrate function overloading.
 */
+
+#include <iostream>
+using namespace std;
+
+int add(int x, int y) {
+    return x + y;
+}
+
+double add(double x, double y) {
+    return x + y;
+}
+
+int main() {
+    cout << add(3, 4) << '\n';
+    cout << add(3.0, 4.0) << '\n';
+
+    return 0;
+}
+
+/*
+7
+7
+*/
