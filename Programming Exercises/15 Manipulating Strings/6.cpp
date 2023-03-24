04d28fc49aee8c77af2d60c44c99fbfea8cab5d7
anupam <tah.anupam@gmail.com>
2023-03-24 17:30:52 +0000

    24 Mar 2023, 23:00

--- Programming Exercises/15 Manipulating Strings/6.cpp
+++ Programming Exercises/15 Manipulating Strings/6.cpp
@@ -1,5 +1,26 @@
 /*
 Write a program that reads the following text and counts the number of times the word "It" appears in it.
 It is new. It is singular.
-It is simple. It must succed!
+It is simple. It must succeed!
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string text = "It is new. It is singular. It is simple. It must succeed!";
+    string word = "It";
+    int count = 0;
+    size_t pos = 0;
+    while ((pos = text.find(word, pos)) != string::npos) {
+        ++count;
+        pos += word.length();
+    }
+    cout << "Number of occurrences of '" << word << "': " << count << '\n';
+    return 0;
+}
+
+/*
+Number of occurrences of 'It': 4
 */
---
+++ Programming Exercises/15 Manipulating Strings/7.cc
@@ -0,0 +1,12 @@
+/*
+Write a program that reads a list of countries in random order 
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    return 0;
+}
