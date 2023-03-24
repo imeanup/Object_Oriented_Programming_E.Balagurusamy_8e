dd78348784bbe349b3a026fe31e09cb4166ebb51
anupam <tah.anupam@gmail.com>
2023-03-24 16:55:26 +0000

    24 Mar 2023, 22:25

---
+++ Programming Exercises/15 Manipulating Strings/1.cpp
@@ -0,0 +1,8 @@
+/*
+ Write a program that reads the name
+ Martin Luther King
+ from the keyboard into three separate string objects and then concatenates them into a new string object using 
+ a) + operator and
+ b) append() function.
+*/
+
