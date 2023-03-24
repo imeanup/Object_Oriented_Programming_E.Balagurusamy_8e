e23c9b4fa75b0cfe95d0b31bc6b458d77807df9c
anupam <tah.anupam@gmail.com>
2023-03-24 17:18:11 +0000

    24 Mar 2023, 22:48

--- Programming Exercises/15 Manipulating Strings/4.cpp
+++ Programming Exercises/15 Manipulating Strings/4.cpp
@@ -7,5 +7,15 @@ Write a program that will read a line of text containing more than three words a
 using namespace std;
 
 int main(){
-    
+    string text;
+    cout << "Enter a line of text: ";
+    getline(cin, text);
+    string :: iterator it;
+    for (it = text.begin(); it != text.end(); ++it){
+        if (*it == ' '){
+            *it = '_';
+        }
+    }
+    cout << "Modified text: " << text << endl;
+    return 0;
 }
