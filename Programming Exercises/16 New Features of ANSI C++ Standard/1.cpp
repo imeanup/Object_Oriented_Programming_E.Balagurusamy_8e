1c67c65fb4976ffb9c685810754d0010cbe64f62
anupam <tah.anupam@gmail.com>
2023-03-24 18:31:32 +0000

    25 Mar 2023, 00:01

--- Programming Exercises/16 New Features of ANSI C++ Standard/1.cpp
+++ Programming Exercises/16 New Features of ANSI C++ Standard/1.cpp
@@ -1,3 +1,18 @@
 /*
 Write a program in which increment in pointer to integer should demonstrate increment in interger using reinterpret_cast operator.
 */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x = 10;
+    int *p = &x;
+    cout << "x = " << x << endl;
+    cout << "p = " << p << endl;
+
+    p = reinterpret_cast<int*>(reinterpret_cast<char*>(p) + sizeof(int));
+    cout << "p(after) = " << p << endl;
+    cout << "*p(after) = " << *p << endl;
+    return 0;
+}
